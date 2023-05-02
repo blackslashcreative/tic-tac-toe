cec594c44a3edaeaf4ee974365dc181fcd7ae33d
Ghosty <ghosty@Ghostys-MacBook-Air.local>
2023-05-02 16:06:26 -0700

    adds squares and changes their color on click

--- colorGame00.js
+++ colorGame00.js
@@ -1,14 +1,29 @@
+const Square = ({id, player}) => {
+  const [color, setColor] = React.useState("green");
+  const palet = ["red", "blue", "green"];
+  const getRandomColor = () => palet[Math.floor(Math.random()*3)];
+  return (
+    <button onClick={(e) => {
+      setColor(getRandomColor());
+      e.target.style.background = color;
+    }}>
+      <h1>{id}</h1>
+    </button>
+  )
+}
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
+  function renderSquare(i) {
+    return <Square id={i} player={player}></Square>;
+  }
   return (
-    <div
-      className="game-board"
-      onClick={(e) => {
-        setPlayer(1 - player); // the player needs to be 0 or 1
-        status = `Player ${player}`;
-      }}
-    >
+    <div className="game-board">
+      <div className="grid-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
